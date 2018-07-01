73b502af026cb79c6ff056458f48a0d8010c6361
okwuokenye <okwuokenye@gmail.com>
2018-07-01 15:25:45 -0400

    Init

---
+++ NewCDT/ViewModels/CharacterViewerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCDT.ViewModels
+{
+    class CharacterViewerViewModel
+    {
+        #region Events
+
+        #endregion
+
+        #region private variables
+        string _ImageSource = string.Empty;
+        #endregion
+
+        #region properties
+        public string ImageSource { get { return _ImageSource; } }
+        #endregion
+
+        #region Constructor
+        public CharacterViewerViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region private methods
+
+        #endregion
+
+        #region public methods
+        public void SetImage(string p_ImageSource)
+        {
+            _ImageSource = p_ImageSource;
+            
+        }
+        #endregion
+    }
+}
