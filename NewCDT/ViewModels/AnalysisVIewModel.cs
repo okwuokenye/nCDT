64088c95fd1dc79a940ad09b8d3e889972f472da
Administrator <Administrator@WIN-54M37RMDDDA>
2018-07-03 07:03:46 -0700

    Added analysis control

---
+++ NewCDT/ViewModels/AnalysisVIewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCDT.ViewModels
+{
+    class AnalysisViewModel : ObservableObject
+    {
+        #region Events
+
+        #endregion
+
+        #region private variables
+        string _ImageSource = string.Empty;
+        #endregion
+
+        #region properties
+        public string ImageSource { get { return _ImageSource; } }
+        #endregion
+
+        #region Constructor
+        public AnalysisViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region private methods
+
+        #endregion
+
+        #region public methods
+        public void SetImage(string p_ImageSource)
+        {
+            _ImageSource = p_ImageSource;
+            RaisePropertyChanged("ImageSource");
+        }
+        #endregion
+    }
+}
